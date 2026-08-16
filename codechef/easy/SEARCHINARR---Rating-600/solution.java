e9db227528e957fd9011dc56f2d0980c6229da0f
Tejas Sainadh Kumar Kabotula <103375544+TejasKabotula@users.noreply.github.com>
2026-08-16 19:11:53 +0530

    ✅ SEARCHINARR - Rating 600 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/easy/SEARCHINARR---Rating-600/solution.java
@@ -0,0 +1,10 @@
+public static String solve(int N, int X, int[] A) {
+        for(int x1:A)
+        {
+            if(X==x1)
+            return "YES";
+           
+        }
+         
+            return "NO";
+}
