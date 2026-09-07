21d061dd0daf3e49c3693246c5eeca479e3a28e9
Tejas Sainadh Kumar Kabotula <103375544+TejasKabotula@users.noreply.github.com>
2026-09-07 12:08:23 +0530

    ✅ SUMNEQ - Rating 592 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-07

---
+++ codechef/easy/SUMNEQ---Rating-592/solution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        // Your code goes here
+        System.out.println(n-1);
+    }
+}
