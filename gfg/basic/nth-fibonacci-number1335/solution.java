968a1f53117e2b81f3490a231bfd39eb1e8b69e8
Tejas Sainadh Kumar Kabotula <103375544+TejasKabotula@users.noreply.github.com>
2026-09-14 22:08:54 +0530

    ✅ Nth Fibonacci Number | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-14

---
+++ gfg/basic/nth-fibonacci-number1335/solution.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+
+class Solution {
+    public int nthFibonacci(int n) {
+        // code here
+         if(n==1)
+            return 1;
+        if(n==2)
+            return 1;
+        int a=0,b=1,c=0;
+        for (int i = 2; i <=n; i++) {
+            c=a+b;
+            a=b;
+            b=c;
+
+        }
+        return c;
+    }
+}
