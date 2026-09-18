eec984bff1c3875cbda53d43bb1d5b0fef3933db
Tejas Sainadh Kumar Kabotula <103375544+TejasKabotula@users.noreply.github.com>
2026-09-18 17:37:12 +0530

    ✅ While Loop | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-18

---
+++ gfg/basic/while-loop/solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        x++;
+
+        // code here
+        while(x-->0)
+        {
+            System.out.print(x+" ");
+        }
+        
+    }
+}
